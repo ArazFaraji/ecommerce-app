fd5c27909f2767c19f8df99e72368b60d28a35ba
Araz Faraji <arazfaraji@gmail.com>
2020-02-13 16:26:36 -0500

    updated app to redirect to home if a user is signed in

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,4 @@
+import { UserActionTypes } from './user.types'
 // A reducer is a function that gets two properties, it gets a state object which represents the previous state, and then it receives an action. That action is an object with two properties -- a type: 'string' to tell the reducer what specific action is coming through and the action may/may not have a Payload as well. 
 // The state property of the reducer is what the redux store is going to pass to this reducer as the current state when the action gets fired. 
 // Needs a initial state value 
@@ -11,7 +12,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
     // switch statement. Case statement and default value. Based on action.type value (a string) if the case of that action type is what we want, then we render something (the : after case) otherwise by default we just return the state.
     // EVERY SINGLE REDUCER GETS EVERY ACTION FIRED even if not related to this reducer. That is why we use case and default. 
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state, 
                 currentUser: action.payload
