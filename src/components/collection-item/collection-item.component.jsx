07ed5a961c5c5fabd5ad877ce199983dffe21ef3
Araz Faraji <arazfaraji@gmail.com>
2020-02-19 20:46:27 -0500

    added in a cart utility js file to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className='collection-item'>
-        <div
-        className='image' 
-        style={{ 
-            backgroundImage: `url(${imageUrl})`
-        }}
-        />
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-        </div>
-    </div>
-);
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
-export default CollectionItem;
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+    
+    return (
+        <div className='collection-item'>
+            <div
+            className='image' 
+            style={{ 
+                backgroundImage: `url(${imageUrl})`
+            }}
+            />
+            <div className='collection-footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
+        </div>
+    )
+};
+
+const mapDispatchToProps = dispatch => ({
+    // we define the addItem prop we pass to CollectionItem, then we return the dispatch action on us creating our action (our action addItem creator in our cart.actions, and that gets dispatched to Redux)
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
