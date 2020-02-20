d821b328d50e6b0727d0e7b13e74570110d21828
Araz Faraji <arazfaraji@gmail.com>
2020-02-20 14:38:46 -0500

    Updated application with reselect. Created a memoized cart selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
@@ -19,4 +20,18 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+
+// .reduce works as it takes in 2 arguments. The accumulator and the element. As it iterates through an array it adds in what we tell it to from each element. This is adding total quantities together to display the total number of items in the cart. The 0 declares the original value of the accumulator for when it reduces through the first element in the array. 
+
+// Below is called a selector because it lets us select a specific piece of State. 
+// We will be adding in cache from Reselect to avoid a component like this from re-rendering the DOM when its state values haven't changed. 
+
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+//     itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+// });       THIS CAN NOW BE RE-WRITTEN AS BELOW
+
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
