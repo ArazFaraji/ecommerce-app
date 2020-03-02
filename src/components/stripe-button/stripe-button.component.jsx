fcade1e05553d03fee26c83b3d09fcd50dc23019
Araz Faraji <54967124+ArazFaraji@users.noreply.github.com>
2020-03-02 12:55:43 -0500

    Added in stripe payment integration. Updated favicon and title of page.

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    // stripe wants all payments to be in cents. So we convert our USD to cents by a simple * 100. 
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_pf6Q8SYdczif8xQ9MZkZw5p300dJEEkIPs';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='King Clothing Llc'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is ${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
